7d8077da2efa4560cbce2f6d2439589f0360dc83
Aguspike <magorsk@live.com.mx>
2026-04-30 16:32:32 -0600

    Delete gemini-code-1777587413225.js

--- gemini-code-1777587413225.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('PWA instalada correctamente');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Aquí es donde sucede la magia del modo offline (se puede expandir después)
-  e.respondWith(fetch(e.request));
-});
